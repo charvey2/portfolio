a5f0fbdebcbbe960869000d830895c0a3602833d
Cam <79339514+charvey2@users.noreply.github.com>
2023-06-02 23:35:58 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Cameron Harvey',
     description:
       'Cameron Harvey is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: '', // No trailing slash allowed!
+    siteUrl: 'https://main--elegant-melba-8b5cfc.netlify.app/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
